c92105348d1e0cf9964f9feee74c5443790b002f
sanjay <sdas69101@gmail.com>
2022-06-15 17:11:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import "./index.css"
+import Header from "./Components/Header"
+import Home from "./Components/Home"
+
+export const App = () => {
+  return (
+    <div className="app">
+      <Header />  
+      <Home />
+      
+    </div>
+  )
+}
+export default App
+
